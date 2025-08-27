0fa9d37be8c5f9a9753c51ca9e7ac937afafab0c
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-27 14:15:08 +0200

    inserisci prompt per richiesta utente

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //definisci una costante con il costo in base ai kilometri
 const kmPrice = 0.21;
 //chiedi all'utente le informazioni necessarie e salvale in variabili
-
+let kmTrip = prompt("Quanti kilometri devi percorrere?");
+let age = prompt("Quanti anni hai?");
 //verifica se l'età permette di accedere a uno sconto (<18 o >65)
 
 //calcola il costo totale
