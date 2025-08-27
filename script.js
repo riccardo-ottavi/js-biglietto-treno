62f9f0eeabd93397ae3c983a69e99593c5e170c8
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-27 14:27:09 +0200

    finalizza programma

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ let age = prompt("Quanti anni hai?");
 
 tempPrice = kmTrip * kmPrice;
 console.log(tempPrice);
-//verifica se l'età permette di accedere a uno sconto (<18 o >65)
 
-//calcola il costo totale
+//verifica se l'età permette di accedere a uno sconto (<18 o >65) e rimanda a schermo il prezzo finale
+let finalPrice;
 
-//restituisci il costo all'utente
+if (age < 18) {
+    finalPrice = tempPrice * 0.8;
+}
+
+if (age > 65) {
+    finalPrice = tempPrice * 0.6;
+}
+
+let formatResult = finalPrice.toFixed(2);
+console.log("il costo è di €" + formatResult)
