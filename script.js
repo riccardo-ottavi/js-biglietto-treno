67238f4a5e6b0f7104e7a5d3b5f5a6d7bea4b124
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-27 14:17:53 +0200

    gestisci il costo in km prima di applicare sconto età

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const kmPrice = 0.21;
 //chiedi all'utente le informazioni necessarie e salvale in variabili
 let kmTrip = prompt("Quanti kilometri devi percorrere?");
 let age = prompt("Quanti anni hai?");
+
+tempPrice = kmTrip * kmPrice;
+console.log(tempPrice);
 //verifica se l'età permette di accedere a uno sconto (<18 o >65)
 
 //calcola il costo totale
