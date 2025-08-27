2f7c0af7a8bfb63345a3069411613ee844552525
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-27 14:11:36 +0200

    definisci costo per kilometro

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //definisci una costante con il costo in base ai kilometri
-
+const kmPrice = 0.21;
 //chiedi all'utente le informazioni necessarie e salvale in variabili
 
 //verifica se l'età permette di accedere a uno sconto (<18 o >65)
