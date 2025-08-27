71b1080363008a60d37bc62f17a4fd1c76812e27
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-27 14:08:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//definisci una costante con il costo in base ai kilometri
+
+//chiedi all'utente le informazioni necessarie e salvale in variabili
+
+//verifica se l'età permette di accedere a uno sconto (<18 o >65)
+
+//calcola il costo totale
+
+//restituisci il costo all'utente
